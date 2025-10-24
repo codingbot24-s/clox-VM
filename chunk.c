bd93569d533ea1841b6adc4b456272b95cdda671
pfromvoid <primecoder8@gmail.com>
2025-10-24 18:58:29 +0530

    fix for reallocate

--- chunk.c
+++ chunk.c
@@ -23,4 +23,4 @@ void write_chunk (Chunk* chunk,uint8_t byte) {
 void free_chunk (Chunk* chunk) {
     FREE(uint8_t,chunk->data,chunk->capacity);
     init_chunk(chunk);
-}z
+}
--- main.c
+++ main.c
@@ -1,4 +1,11 @@
 #include "common.h"
 #include "stdio.h"
-int main () {
+#include "chunk.h"
+int main (int argc,const char* argv[]) {
+    Chunk chunk;
+    init_chunk(&chunk);
+    write_chunk(&chunk,OP_RETURN);
+    free_chunk(&chunk);
+    
+    return 0;
 }
