905adcd54631f47bd66cc564f08dc10036b07ad9
pfromvoid <primecoder8@gmail.com>
2025-10-26 18:56:43 +0530

    struct done

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include "common.h"
 #include "stdio.h"
 #include "chunk.h"
+#include "debug.h"
+
 int main (int argc,const char* argv[]) {
     Chunk chunk;
     init_chunk(&chunk);
     write_chunk(&chunk,OP_RETURN);
+    disassembleChunk(&chunk,"test chunk");    
+
     free_chunk(&chunk);
-    
+
     return 0;
 }
